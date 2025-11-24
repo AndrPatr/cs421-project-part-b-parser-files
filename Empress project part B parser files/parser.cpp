97088fbdb409994445a75889b5a727af689393f6
Peter421M <112452520+Peter421M@users.noreply.github.com>
2025-11-23 23:34:22 -0800

    Update parser.cpp

--- Empress project part B parser files/parser.cpp
+++ Empress project part B parser files/parser.cpp
@@ -31,14 +31,14 @@ ifstream fin;
 
 /////////////////////////////////////////////////SYNTAX ERROR FUNCTIONS
 // Type of error: ** unexpected token in match()
-// Done by: ** 
+// Done by: ** Andrew
 void syntaxerror1( token_type expected,string lexeme)
 {
   cout<<" Syntax error: expected "<< tokenName[expected] << "but found "<< lexeme <<endl;
   exit (1);
 }
 // Type of error: ** unexpected token in parser function
-// Done by: ** 
+// Done by: ** Andrew
 void syntaxerror2(string functionName, string lexeme)
 {
    cout<< "Syntax error: expected "<<lexeme<< "found in" << functionName<< endl;
@@ -50,7 +50,7 @@ void syntaxerror2(string functionName, string lexeme)
 //************************************** Need the updated match and next_token with 2 global vars
 // saved_token and saved_lexeme
 // Purpose: **fetch next token from scanner unless 1 is saved already
-// Done by: **
+// Done by: ** Andrew
 token_type next_token()
 {
  if( !token_available)
@@ -62,7 +62,7 @@ token_type next_token()
 }
 
 // Purpose: **return the saved token and reset the saved variables
-// Done by: **
+// Done by: ** Andrew
 boolean match(tokentype expected) 
 {
    if (next_token() != expected)
@@ -85,14 +85,181 @@ boolean match(tokentype expected)
 // ** Be sure to put the corresponding grammar rule above each function
 // ** Be sure to put the name of the programmer above each function
 
+// Grammar: ** <story> ::= <s> { <s> }
+// Done by: ** Peter Morsch
+void story() 
+{
+   cout << "Processing <story>" << endl;
+   s();
+   while(true) 
+   {
+      switch (next_token()) 
+      {
+         case CONNECTOR:
+         case WORD1:
+         case PRONOUN: 
+            s();
+            break;
+         default:
+            return; 
+      }
+   }
+}
 
+// Grammar: ** <s> ::= [CONNECTOR] <noun> SUBJECT <after subject>
+// Done by: ** Peter Morsch
+void s()
+{
+   cout << "Processing <s>" << endl;
+   if(next_token() == CONNECTOR)
+   {
+      match(CONNECTOR);
+   }
+   noun();
+   match(SUBJECT);
+   aftersubject();
+}
 
+// Grammar: ** <after subject> ::= <verb> <tense> PERIOD | <noun> <after noun>
+// Done by: ** Peter Morsch
+void aftersubject()
+{
+   cout << "Processing <aftersubject>" << endl;
+   switch (next_token()) 
+   {
+      case WORD2:
+         verb();
+         tense();
+         match(PERIOD);
+         break;
+      case WORD1:
+      case PRONOUN:
+         noun();
+         afternoun();
+         break;
+      default:
+         syntaxerror2("<aftersubject>", saved_lexeme);
+   }
+}
 
+// Grammar: ** <after noun> ::= <be> PERIOD | DESTINATION <verb> <tense> PERIOD | OBJECT <after object>
+// Done by: ** Peter Morsch
+void afternoun()
+{
+   cout << "Processing <afternoun>" << endl;
+   switch (next_token())
+   {
+      case IS:
+      case WAS:
+         be();
+         match(PERIOD);
+         break;
+      case DESTINATION:
+         match(DESTINATION);
+         verb();
+         tense();
+         match(PERIOD);
+         break;
+      case OBJECT:
+         match(OBJECT);
+         afterobject();
+      default:
+         syntaxerror2("<afternoun>", saved_lexeme);
+   }
+}
 
+// Grammar: ** <after object> ::= <verb> <tense> PERIOD | <noun> DESTINATION <verb> <tense> PERIOD
+// Done by: ** Peter Morsch
+void afterobject()
+{
+   cout << "Processing <afterobject>" << endl;
+   switch (next_token()) 
+   {
+      case WORD2:
+         verb();
+         tense();
+         match(PERIOD);
+         break;
+      case WORD1:
+      case PRONOUN:
+         noun();
+         match(DESTINATION);
+         verb();
+         tense();
+         match(PERIOD);
+         break;
+      default:
+         syntaxerror2("<afterobject>", saved_lexeme);
+   }
+}
 
+// Grammar: ** <noun> ::= WORD1 | PRONOUN
+// Done by: ** Peter Morsch
+void noun() 
+{
+   cout << "Processing <noun>" << endl;
+   switch (next_token()) 
+   {
+      case WORD1:
+         match(WORD1);
+         break;
+      case PRONOUN:
+         match(PRONOUN);
+         break;
+      default:
+         syntaxerror2("<noun>", saved_lexeme);
+   }
+}
 
-// Grammar: **
-// Done by: **
+// Grammar: ** <verb> ::= WORD2
+// Done by: ** Peter Morsch
+void verb() 
+{
+   cout << "Processing <verb>" << endl;
+   match(WORD2);
+}
+
+// Grammar: ** <be> ::= IS | WAS
+// Done by: ** Peter Morsch
+void be() 
+{
+   cout << "Processing <be>" << endl;
+   switch (next_token()) 
+   {
+      case IS:
+         match(IS);
+         break;
+      case WAS:
+         match(WAS);
+         break;
+      default:
+         syntaxerror2("<be>", saved_lexeme);
+   }
+}
+
+// Grammar: ** <tense> := VERBPAST | VERBPASTNEG | VERB | VERBNEG
+// Done by: ** Peter Morsch
+void tense() 
+{
+   cout << "Processing <tense>" << endl;
+   switch (next_token()) 
+   {
+      case VERBPAST:
+         match(VERBPAST);
+         break;
+      case VERBPASTNEG:
+         match(VERBPASTNEG);
+         break;
+      case VERB:
+         match(VERB);
+         break;
+      case VERBNEG:
+         match(VERBNEG);
+         break;
+      default:
+         syntaxerror2("<tense>", saved_lexeme);
+   }
+}
 
 
 //----------- Driver ---------------------------
